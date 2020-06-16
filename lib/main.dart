6d87993d893cac0a4d7eda0f335d8ca148a7d9a7
Mohannadov <matrix.xxcs102@gmail.com>
2020-06-17 02:44:17 +0300

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+import 'package:foolife/Screens/Welcome/Welcome_Screen.dart';
+//import 'package:shared_preferences/shared_preferences.dart';
+
+void main() {
+  //SharedPreferences.getInstance().then((prefs) {
+  //runApp(MyApp(prefs: prefs));
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // final SharedPreferences prefs;
+  //MyApp({this.prefs});
+  MyApp();
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      routes: <String, WidgetBuilder>{
+        '/walkthrough': (BuildContext context) => WelcomeScreen(),
+        /* '/root': (BuildContext context) => new RootScreen(),
+        '/signin': (BuildContext context) => new SignInScreen(),
+        '/signup': (BuildContext context) => new SignUpScreen(),
+        '/main': (BuildContext context) => new MainScreen(),*/
+      },
+      theme: ThemeData(
+        primaryColor: Colors.white,
+        primarySwatch: Colors.grey,
+      ),
+      home: _handleCurrentScreen(),
+    );
+  }
+
+  Widget _handleCurrentScreen() {
+    //bool seen = (prefs.getBool('seen') ?? false);
+    /*if (seen) {
+      return new RootScreen();
+    } else {
+      return new WalkthroughScreen(prefs: prefs);
+    }*/
+
+    // return WelcomeScreen(prefs: prefs);
+    return WelcomeScreen();
+  }
+}
